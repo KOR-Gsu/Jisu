4c0b8b13bfe4b7ebf4d3bb01d1021960e977f0fa
Paladin-Danse <dltmdwns1026@gmail.com>
2020-08-24 14:59:59 +0900

    0824
    3D 포폴 착수

---
+++ 제출/ProjectG/Assets/Script/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string moveAxis = "Vertical";
+    public string rotateAxis = "Horizontal";
+    public string attackAxis = "Fire1";
+
+    public float move { private set; get; }
+    public float rotate { private set; get; }
+    public float attack { private set; get; }
+    
+    void Update()
+    { 
+        move = Input.GetAxis(moveAxis);
+        rotate = Input.GetAxis(rotateAxis);
+        attack = Input.GetAxis(attackAxis);
+    }
+}
---
+++ 제출/ProjectG/Assets/Script/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 15f;
+    public float rotateSpeed = 180f;
+
+    private PlayerInput playerInput;
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Rotate();
+        Move();
+
+        playerAnimator.SetFloat("Move", playerInput.move);
+    }
+
+    void Move()
+    {
+        Vector3 moveDistance = moveSpeed * Time.deltaTime * transform.forward * playerInput.move;
+
+        playerRigidbody.MovePosition(moveDistance);
+    }
+
+    void Rotate()
+    {
+        float rotateAngle = rotateSpeed * Time.deltaTime * playerInput.rotate;
+
+        playerRigidbody.rotation *= Quaternion.Euler(0, rotateAngle, 0);
+    }
+}
