ae17585d40dbc52135345f483fec40d2f6b3540d
GSU_J <jp1507@naver.com>
2020-12-08 20:57:56 +0900

    12.08
    portal & Changing Scene

---
+++ 제출/ProjectG/Assets/Script/LoadScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public float fadeDuration = 1.0f;
+    public CanvasGroup fadeCg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.UnloadSceneAsync("VillageScene");
+            SceneManager.LoadSceneAsync("DungeonScene");
+        }
+    }
+}
