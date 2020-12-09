f81bd13b9722d15ffd4bb6d1ccb638e116e531a5
GSU_J <jp1507@naver.com>
2020-12-09 20:52:28 +0900

    12.09
    Enemy 추가
    Animator 수정 및 플레이어 체력 추가

---
+++ 제출/ProjectG/Assets/Script/Enemy.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : LivingEntity
+{
+    public LayerMask TargetLayer;
+
+    private LivingEntity targetEntity;
+    private NavMeshAgent pathFinder;
+
+    private Animator enemyAnimator;
+
+    public float damage = 5f;
+    public float timeBetAttack = 0.5f;
+    public float lastAttackTime;
+
+    private bool hasTarget
+    {
+        get
+        {
+            if (targetEntity != null && !targetEntity.dead)
+                return true;
+
+            return false;
+        }
+    }
+
+    public void Setup(float newHealth, float newDamage, float newSpeed)
+    {
+        startingHealth = newHealth;
+        damage = newDamage;
+        pathFinder.speed = newSpeed;
+    }
+
+    void Awake()
+    {
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UpdatePath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyAnimator.SetBool("hasTarget", hasTarget);
+    }
+
+    private IEnumerator UpdatePath()
+    {
+        while(!dead)
+        {
+            if(hasTarget)
+            {
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                pathFinder.isStopped = true;
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 20f, TargetLayer);
+
+                for(int i =0;i<colliders.Length;i++)
+                {
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    if(livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+                        break;
+                    }
+
+                }
+            }
+
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    public override void OnDamage(float damage, Vector3 hitPoint)
+    {
+        base.OnDamage(damage, hitPoint);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        Collider[] enemyColliders = GetComponents<Collider>();
+        for (int i = 0; i < enemyColliders.Length; i++)
+            enemyColliders[i].enabled = false;
+
+        pathFinder.isStopped = true;
+        pathFinder.enabled = false;
+
+        enemyAnimator.SetTrigger("Die");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(!dead && Time.time >= lastAttackTime + timeBetAttack)
+        {
+            LivingEntity attackTarget = other.GetComponent<LivingEntity>();
+
+            if(attackTarget != null && attackTarget == targetEntity)
+            {
+                lastAttackTime = Time.time;
+
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+
+                attackTarget.OnDamage(damage, hitPoint);
+            }
+        }
+    }
+}
---
+++ 제출/ProjectG/Assets/Script/LivingEntity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public float startingHealth = 100f;
+    public float health { get; protected set; }
+    public bool dead { get; protected set; }
+    public event Action onDeath;
+
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = startingHealth;
+    }
+
+    public virtual void OnDamage(float damage, Vector3 hitPoint)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead)
+            Die();
+    }
+
+    public virtual void RestoreHealth(float newHealth)
+    {
+        if (!dead)
+            health += newHealth;
+    }
+
+    public virtual void Die()
+    {
+        if (onDeath != null)
+            onDeath();
+
+        dead = true;
+    }
+}
