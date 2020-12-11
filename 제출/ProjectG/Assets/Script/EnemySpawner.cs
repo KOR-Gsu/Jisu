2229db03a7f7922521948cb2e565c66651067344
GSU_J <jp1507@naver.com>
2020-12-11 20:54:58 +0900

    12.11
    Enemy Spawner 추가 및 수정요망

---
+++ 제출/ProjectG/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemyPrefab;
+    public Transform[] spawnPoints;
+
+    public float damageMax = 7;
+    public float damageMin = 2;
+
+    public float hpMax = 150;
+    public float hpMin = 80;
+
+    public float speedMax = 5;
+    public float speedMin = 3;
+
+    public Color strongEnemyColor = Color.green;
+
+    private List<Enemy> enemies = new List<Enemy>();
+    private int wave = 0;
+
+    void Update()
+    {
+        if (GameManager.instance != null && GameManager.instance.isGameOver)
+            return;
+
+        if(enemies.Count <= 0)
+        {
+            
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        wave++;
+
+        int spawnCount = Mathf.RoundToInt(wave * 1.5f);
+
+        for(int i =0; i< spawnCount;i++)
+        {
+            float intensity = Random.Range(0, 1.0f);
+            CreateEnemy(intensity);
+        }
+    }
+
+    private void CreateEnemy(float intensity)
+    {
+        float hp = Mathf.Lerp(hpMin, hpMax, intensity);
+        float damage = Mathf.Lerp(damageMin, damageMax, intensity);
+        float speed = Mathf.Lerp(speedMin, speedMax, intensity);
+
+        Color skinColor = Color.Lerp(Color.white, strongEnemyColor, intensity);
+
+
+    }
+}
---
+++ 제출/ProjectG/Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager m_instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if (instance == null)
+                m_instance = FindObjectOfType<GameManager>();
+
+            return m_instance;
+        }
+    }
+    public bool isGameOver { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        FindObjectOfType<PlayerHP>().onDeath += EndGame;
+    }
+    
+    void EndGame()
+    {
+        isGameOver = true;
+    }
+}
